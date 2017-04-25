883f7da945577341a905c2261b9aa9f0c92c8bca
James Jarvis <jamesjarvis501@gmail.com>
2017-04-25 22:02:41 +0100

    test

--- includes/contact.php
+++ includes/contact.php
@@ -1,23 +1,24 @@
 <div class="content">
     <div class="row blue center">
-        <h1>Contact us and tell us your story</h1>
-        <p>If you would like to tell us your WWII stories, please fill in the simple form below or arrange a meeting with one of our team members, and we can help you upload your story and communicate with others.</p>
+        <h1 style="margin-top:10px;">Contact us and tell us your story</h1>
+        <p style="margin-bottom:10px;">If you would like to tell us your WWII stories, please fill in the simple form below or arrange a meeting with one of our team members, and we can help you upload your story and communicate with others.</p>
     </div>
 
 
     <div class="row blue center">
-        <h1>Read the stories below</h1>
-        <p>Press on the button under each story to read in full, and to see the information of the person who submitted it.</p>
+        <h1 style="margin-top:10px;">Read the stories below</h1>
+        <p style="margin-bottom:10px;">Press on the button under each story to read in full, and to see the information of the person who submitted it.</p>
     </div>
-    <div class="memory-wall">
+    <div class="memory-wall" style="height:230px;">
         <div class="arrow-left">
             <i class="fa fa-chevron-left" onclick="plusDivs(-1)"aria-hidden="true"></i>
         </div>
         <div id="imgData" style="display:none;">
-            <div class="mySlides" style="background-color:#004579;color:#fff;">
+            <div class="mySlides" style="background-color:#004579;color:#fff;height:200px;">
                 Mary Jones
                 <br />
-                Story: My time in the womens' land army, at just 16, I was thrown in at...
+                <p>Story: My time in the womens' land army, at just 16, I was thrown in at...</p>
+                <br />
                 <a href="story">READ MORE</a>
             </div>
         </div>
