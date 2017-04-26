5e23f2304245ac3af63f4f47bec3e91410489c04
James Jarvis <jamesjarvis501@gmail.com>
2017-04-26 19:01:40 +0100

    test

--- includes/story.php
+++ includes/story.php
@@ -1,20 +1,4 @@
 <div class="row blue">
 <?php
-include_once('../php/connect.php');
-$stmt = $conn->prepare("SELECT Name, Story FROM VeteranDetails WHERE VetID = :VetID ORDER BY DateAdded DESC");
-$stmt->bindParam(':VetID', $_GET['id']);
-$stmt->execute();
-if ($stmt->execute()) {
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo '<h1 style="font-size: 26pt;margin-top: 0;padding-top: 30px;padding-bottom:30px;text-align: center;">';
-        echo $row['Name'];
-        echo '</h1>';
-        echo '</div>';
-        echo '<div class="row">';
-        echo '<p>'.$row['Story'].'</p>';
-        echo '</div>';
-    }
-}
-$conn = null;
-$stmt = null;
+    include('./php/get_story');
 ?>
