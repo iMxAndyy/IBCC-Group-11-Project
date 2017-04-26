5ecd8ca8f9bcfa44f080d8128aa34f0778b27d04
James Jarvis <jamesjarvis501@gmail.com>
2017-04-26 10:44:26 +0100

    test

---
+++ includes/upload.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="column2">
+        <div class="upload">
+            <form action="/php/save_image.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+        </div>
+    </div>
+    <div class="column2">
+        Upload an image from your computer, phone or tablet. Simply press on the upload button, and find the image you wish to upload.
+    </div>
+</div>
--- upload.php
+++ upload.php
@@ -13,6 +13,7 @@
 
     <?php
     include('includes/header.html');
+    include('includes/upload.html');
     include('includes/footer.html');
     ?>
 
