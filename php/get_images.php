7fe806eca6067f812585f4b8f6f0aceea1ec7c94
James Jarvis <jamesjarvis501@gmail.com>
2017-04-26 17:30:33 +0100

    should be ok

--- php/get_images.php
+++ php/get_images.php
@@ -7,7 +7,7 @@ $stmt->execute();
 
 if ($stmt->execute()) {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo '<div class="mySlides" style="cursor:pointer;" onclick="changeLargeImage('."'".row[ImgDir]."'".')" style="background-image:url(.'.$row[ImgDir].'"></div>';
+        echo '<div class="mySlides" style="cursor:pointer;" onclick="changeLargeImage('."'".$row[ImgDir]."'".')" style="background-image:url(.'.$row[ImgDir].'"></div>';
     }
 }
 
