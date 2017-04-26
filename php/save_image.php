9925b35e7cf047e7daa30222a56ea4481a92138d
James Jarvis <jamesjarvis501@gmail.com>
2017-04-26 12:02:48 +0100

    should work now

--- php/save_image.php
+++ php/save_image.php
@@ -1,4 +1,5 @@
 <?php
+
 function generateRandomString($length = 10) {
     return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
 }
@@ -6,50 +7,51 @@ function generateRandomString($length = 10) {
 include_once('connect.php');
 
 $target_dir = "../data/img/";
-$imgKey = generateRandomString();
+$imageFileType = pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION);
+$imgKey = generateRandomString().".".$imageFileType;
 $imgDir = "/data/img/" . $imgKey;
 $target_file = $target_dir . $imgKey;
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
+    if ($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
         $uploadOk = 0;
     }
-}
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-
-        $stmt = $conn->prepare("INSERT INTO Images (ImgDir, DateUploaded) VALUES (:ImgDir, NOW())");
-        $stmt->bindParam(':ImgDir', $ImgDir);
-        $ImgDir=$imgDir;
-        $stmt->execute();
-        $conn = null;
-        $stmt = null;
-
-        header('Location:/index.php');
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+            $stmt = $conn->prepare("INSERT INTO Images (ImgDir, DateUploaded) VALUES (:ImgDir, NOW())");
+            $stmt->bindParam(':ImgDir', $ImgDir);
+            $ImgDir = $imgDir;
+            $stmt->execute();
+            $conn = null;
+            $stmt = null;
+
+            header('Location:/index.php');
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
     }
 }
 
