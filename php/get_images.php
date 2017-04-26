956a8cc2c67e3ac31c169fbc304d13918b508cd4
James Jarvis <jamesjarvis501@gmail.com>
2017-04-26 11:42:55 +0100

    test

--- php/get_images.php
+++ php/get_images.php
@@ -5,16 +5,14 @@ include_once('connect.php');
 $stmt = $conn->prepare("SELECT ImgID, ImgDir FROM Images ORDER BY DateUploaded DESC");
 $stmt->execute();
 
-
 if ($stmt->execute()) {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         echo $row[ImgDir];
-        echo '<div class="mySlides" style="background-image:url('.$row[ImgDir].'"></div>';
+        echo '<div class="mySlides" style="background-image:url(.'.$row[ImgDir].'"></div>';
     }
 }
 
 $conn = null;
 $stmt = null;
-
 
 ?>
